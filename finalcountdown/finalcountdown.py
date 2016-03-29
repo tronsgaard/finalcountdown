47c117aec684179f1d9669b6281ca25223ae58e7
René Tronsgaard Rasmussen <tronsgaard@gmail.com>
2016-03-29 03:08:59 +0200

    Added non-plural labels and did some minor improvements

--- finalcountdown/finalcountdown.py
+++ finalcountdown/finalcountdown.py
@@ -11,7 +11,7 @@ from gi.repository import AppIndicator3 as appindicator
 
 # Config
 FINAL_DATE = date(2016,6,16)  # The date we are counting down to
-REFRESH_TIME = 60  # seconds
+REFRESH_INTERVAL = 60  # seconds
 
 class FinalCountdown:
 
@@ -32,27 +32,44 @@ class FinalCountdown:
     # Define menu
     menu = Gtk.Menu()
 
-    menu_item = Gtk.MenuItem("{:%A, %d %B %Y}".format(self.finaldate))
-    menu.append(menu_item)
+    menu_item = Gtk.MenuItem("Time is flying..")
     menu_item.set_sensitive(0)
     menu_item.show()
+    menu.append(menu_item)
+    
+
+    menu_item = Gtk.MenuItem("{:%A, %d %B %Y}".format(self.finaldate))
+    menu_item.set_sensitive(0)
+    menu_item.show()
+    menu.append(menu_item)
 
     menu_item = Gtk.MenuItem("Quit")
-    menu.append(menu_item)
-    menu_item.connect("activate", Gtk.main_quit)
+    menu_item.connect("activate", self.quit)
     menu_item.show()
+    menu.append(menu_item)
 
     self.indicator.set_menu(menu)
 
   def update(self):
     # Calculate the difference in days
     delta = self.finaldate - date.today()
-    days = delta.days if delta.days > 0 else 0
+    days = delta.days
     # Update the indicator
-    self.indicator.set_label("{} days..".format(days), '0 days..')
+    if days > 1:
+      label = "{} days..".format(days)
+    elif days == 1:
+      label = "1 day..".format(days)
+    elif days == 0:
+      label = "It's today!"
+    else:
+      print('Exiting (final date is in the past)')
+      self.quit()
+    self.indicator.set_label(label, '')
     # Set timer for next update
-    GObject.timeout_add(REFRESH_TIME * 1000, self.update)
+    GObject.timeout_add(REFRESH_INTERVAL * 1000, self.update)
 
+  def quit(self, *args):
+    Gtk.main_quit()
 
 if __name__ == "__main__":
   signal.signal(signal.SIGINT, signal.SIG_DFL)
